119a12368abe466239ba9c718766732d9c21aaeb
R. LaRoi <are@raewhoo.com>
2024-05-24 16:08:32 -0400

    Update dataCollect.js

--- Javascript/dataCollect.js
+++ Javascript/dataCollect.js
@@ -1,3 +1,5 @@
+// ---- Part One
+
 const csvData =
   "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
@@ -15,4 +17,21 @@ let cellThree = csvArray[3].split(",");
 
 console.log(cellOne);
 
-//  ---------------
+//  PART TWO: ---------------
+
+//  declare a variable that stores the number of columns in each row;
+
+let csvColumn0 = cellZero.length;
+let csvColumn1 = cellOne.length;
+let csvColumn2 = cellTwo.length;
+let csvColumn3 = cellZero.length;
+
+console.log(csvColumn0);
+
+//  create parent array
+
+let parentArray = [];
+
+parentArray.push(cellZero, cellOne, cellTwo, cellThree);
+
+console.log(parentArray);
