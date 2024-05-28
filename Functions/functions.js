08cc8157db314aea5f38f7b2621bac490f4cb00a
R. LaRoi <are@raewhoo.com>
2024-05-28 11:47:43 -0400

    functions.js

--- Functions/functions.js
+++ Functions/functions.js
@@ -28,3 +28,49 @@ let planetHasWater = function (planet) {
 };
 
 console.log(planetHasWater("Earth"));
+
+// parameters are the placeholders for inputs that the fn will recieve
+//  arguments are specific values of JS data types
+
+function maxOfTwoNumbers(n1, n2) {
+  if (n1 > n2) {
+    return n1;
+  } else if (n2 > n1) {
+    return n2;
+  } else if (n1 === n2) {
+    return n1;
+  }
+}
+
+console.log(maxOfTwoNumbers(53, 33));
+
+//a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them.
+function maxOfThree(n1, n2, n3) {
+  if (n1 > n2 && n1 > n3) {
+    return n1;
+  } else if (n2 > n1 && n3 < n2) {
+    return n2;
+  } else if (n3 > n2 && n3 > n1) {
+    return n3;
+  }
+}
+
+console.log(maxOfThree(500, 80, 9));
+
+// a function declaration, isCharAVowel that takes a character as an argument and returns true if it is a vowel, false otherwise.
+
+function isCharAVowel(character) {
+  if (
+    character === "a" ||
+    character === "e" ||
+    character === "i" ||
+    character === "o" ||
+    character === "u"
+  ) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+
+isCharAVowel("i");
