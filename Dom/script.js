21fe9c8c318c97e1bb6725bc5f0aac0a0a4a9cb2
R. LaRoi <are@raewhoo.com>
2024-06-03 19:37:43 -0400

    Dom

--- Dom/script.js
+++ Dom/script.js
@@ -1,3 +1,11 @@
+// Menu data structure
+var menuLinks = [
+  { text: "about", href: "/about" },
+  { text: "catalog", href: "/catalog" },
+  { text: "orders", href: "/orders" },
+  { text: "account", href: "/account" },
+];
+
 // Part 1 ------
 
 // 1. select and cache main element ----
@@ -11,15 +19,36 @@ mainEl.style.background = "var(--main-bg)";
 mainEl.innerHTML = `<h1> DOM MANIPULATION </h1>`;
 
 // 4. add class flex-ctr
-
 mainEl.classList.add("flex-ctr");
 
-//Part 2: Create Menu
+//Part 2: Create Menu ---------
 
 // 1.  select nav
 let topMenuEl = document.getElementById("top-menu");
-console.log(topMenuEl);
 
 // 2. set height
+topMenuEl.style.height = "100%";
+// topMenuEl.setProperty("background", "var(--top-menu-bg)");
 
-topMenuEl.setAttribute(height, "100%");
+// 3. add background color
+topMenuEl.style.background = "var(--top-menu-bg";
+
+// 4. add flex class
+topMenuEl.classList.add("flex-around");
+console.log(topMenuEl);
+
+// Part 3: Adding Menu Buttons
+
+// 1. iterate over links data
+
+let theLinks = menuLinks.map((aLink) => {
+  // 2. create an a element
+  // 3. add href
+  let aTag = document.createElement("a");
+  aTag.href = aLink.href;
+  aTag.textContent = aLink.text;
+  console.log(aTag);
+
+  // 5. append element
+  topMenuEl.append(aTag);
+});
