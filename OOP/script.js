af8ec12938aa55cab4e132ecaba9697f2a718d4d
R. LaRoi <are@raewhoo.com>
2024-06-14 13:13:09 -0400

    Update script.js

--- OOP/script.js
+++ OOP/script.js
@@ -4,7 +4,12 @@ const adventurer = {
   inventory: ["sword", "potion", "artifact"],
 };
 
+//  log the inventory
 for (let index = 0; index < adventurer.inventory.length; index++) {
   const item = adventurer.inventory[index];
   console.log(item);
 }
+
+//  add companion
+adventurer.companion = { name: "Leo", type: "Cat" };
+console.log(adventurer);
