1ede5cdbdbb36f7ada9da2b516222ab0abb089c9
R. LaRoi <are@raewhoo.com>
2024-06-20 22:23:44 -0400

    style.css

--- DataFetch/axios-script.js
+++ DataFetch/axios-script.js
@@ -1,70 +1,109 @@
-// import * as Carousel from "./Carousel.js";
-// import axios from "axios";
-
 const breedSelect = document.getElementById("breedSelect");
 
 const infoDump = document.getElementById("infoDump");
 
-const progressBar = document.getElementById("progressBar");
+const progressBar = document.getElementById("progress-bar");
 
 const getFavouritesBtn = document.getElementById("getFavouritesBtn");
 
-let carousel = document.getElementById("car-img");
+let carousel = document.getElementById("cat-column");
 
 let API_KEY =
   "live_pSmjFQzPtXrYMiFp0YUHtvsMopyYu9WtRlGN5YMsY0kBnrvMaQJegT69HodpyRd2";
 
 async function initialLoad() {
-  catUrl = `https://api.thecatapi.com/v1/breeds`;
+  try {
+    const defaultOptions = {
+      baseURL: "https://api.thecatapi.com/v1/breeds",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-  await axios.get(catUrl).then((response) => {
-    let catData = response.data;
+    const addProgressBar = {
+      onUploadProgress: function updateProgress(progressEvent) {
+        let proPercent =
+          Math.floor(progressEvent.loaded / progressEvent.total) * 100;
 
-    catData.forEach((cat) => {
-      let option = document.createElement("option");
+        progressBar.setAttribute("value", proPercent);
 
-      option.setAttribute("value", cat.id);
-      option.textContent = cat.name;
-      breedSelect.append(option);
-    });
+        console.log(proPercent);
+      },
+    };
 
-    breedSelect.addEventListener("change", (e) => {
-      e.preventDefault();
-      let item = document.createElement("div");
-      async function fetchCat() {
-        let catBreed = breedSelect.value;
-        console.log(catBreed);
+    await axios.get(defaultOptions.baseURL, addProgressBar).then((response) => {
+      let catData = response.data;
 
-        await axios
-          .get(
-            `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${catBreed}&api_key=${API_KEY}`
-          )
-          .then((response) => {
-            let catData = response.data;
-            return catData;
-          })
-          .then((catBreed) => {
-            catBreed.map((element) => {
-              console.log(element);
+      axios.interceptors.request.use(
+        function (config) {
+          console.time(config.url);
+          progressBar.style.width = "0%";
+          document.body.style.cursor = "progress";
 
-              let catImage = element.url;
-              document.getElementById("this-cat").setAttribute("src", catImage);
-              console.log(catImage);
-              carousel.append(catImage);
+          return config;
+        },
+        function (error) {
+          return Promise.reject(error);
+        }
+      );
 
-              item.innerHTML = `<h3>${element.breeds[0].name}</h3> <p>${element.breeds[0].description}</p><p>${element.breeds[0].temperament}</p>`;
-              carousel.append(catImage);
+      axios.interceptors.response.use(
+        (response) => {
+          console.timeEnd(response.config.url);
+          console.log(response);
+          document.querySelector("body").style.cursor = "default";
+          progressBar.style.width = "100%";
+          return response;
+        },
+        (error) => {
+          console.timeEnd(error.response.config.url);
+          return Promise.reject(error);
+        }
+      );
+
+      catData.forEach((cat) => {
+        let option = document.createElement("option");
+        option.setAttribute("value", cat.id);
+        option.textContent = cat.name;
+        breedSelect.append(option);
+      });
+
+      breedSelect.addEventListener("change", (e) => {
+        e.preventDefault();
+
+        async function fetchCat() {
+          let catBreed = breedSelect.value;
+          console.log(catBreed);
+
+          await axios
+            .get(
+              `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${catBreed}&api_key=${API_KEY}`
+            )
+            .then((response) => {
+              let catData = response.data;
+              return catData;
+            })
+            .then((catBreed) => {
+              catBreed.map((element) => {
+                console.log(element);
+
+                let catImage = element.url;
+                document
+                  .getElementById("this-cat")
+                  .setAttribute("src", element.url);
+
+                console.log(catImage);
+
+                infoDump.innerHTML = `<h3>${element.breeds[0].name}</h3> <p>${element.breeds[0].description}</p><p>${element.breeds[0].temperament}</p>`;
+              });
             });
-            infoDump.appendChild(item);
-          });
-      }
-      fetchCat();
+        }
+        fetchCat();
+      });
     });
-  });
+  } catch (error) {
+    console.error(error);
+  }
 }
-// let catData = response.data;
-//
-//
-// let catData = fetchCat.json();
 
 initialLoad();
